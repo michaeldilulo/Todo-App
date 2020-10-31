8e742e41bbc187daa052fae59f65a7b783f7597f
michael dilulo <snoleopard01001101@gmail.com>
2020-10-31 13:29:37 -0700

    todos.map rendering all todos on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import Todo from './Todo';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // short term memory
+  // call the list: todos, need a way of updating list: setTodos
+  const [todos, setTodos] = useState([
+    {
+      title: "Take out the trash",
+      description: "The bin man comes at 9PM"
+    },
+    {
+      title: "Walk the dog",
+      description: "She is bored"
+    },
+    {
+      title: "Go Food shopping",
+      description: "No food in the fridge"
+    }
+  ])
+
   return (
     <div className="app">
-      <Todo title="Take out the trash" description="The bin man comes at 9PM" />
-      <Todo title="Walk the dog" description="She is bored" />
-      <Todo title="Go Food shopping" description="No food in the fridge" />
+      {
+        todos.map((todo) => (
+          <Todo title={todo.title} description={todo.description} />
+        ))
+      }
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./Todo.css"
 
-// Destructuring: If you do this, you cannot clg the props
-const Todo = ({ title, description }) => {
+const Todo = (props) => {
     return (
         <div className="todo">
-            <h1>{title}</h1>
-            <p>{description}</p>
+            <h1>{props.title}</h1>
+            <p>{props.description}</p>
         </div>
     )
 }
