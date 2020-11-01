820016035eee5b71a98546f41b68118107670a8a
michael dilulo <snoleopard01001101@gmail.com>
2020-11-01 10:19:47 -0800

    Todo Form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 
-const TodoForm = () => {
+// Properties of the Todo for my Todo Object
+const TodoForm = (props) => {
     const [input, setInput] = useState('')
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const submitTodoForm = (event) => {
+        event.preventDefault();
+
+        // Able to access the props of the todo
+        props.onSubmit = ({
+            id: Math.floor(Math.random() * 10000 + 1),
+            text: input
+        })
+        /* setInput to empty string: This clears out the input field when you
+        submit the form
+        */
+        setInput('')
+    }
 
     return (
         <div>
-            <form className="todo-form">
+            <form className="todo-form" onSubmit={submitTodoForm}>
                 <input type="text" placeholder="Add Todo" value={input}
                     name="text" className="todo-input"
+                    onChange={handleChange}
                 />
-                <button className="todo-button">Add Todo</button>
+                <button className="todo-button" type="submit">Add Todo</button>
             </form>
         </div>
     )
